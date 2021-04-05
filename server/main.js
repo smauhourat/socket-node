0748ea97dd85cc4c48a49afed25c28be6ffdc51d
smauhourat <santiagomauhourat@hotmail.com>
2021-04-05 10:39:46 -0300

    initial

---
+++ server/main.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var messages = [{
+    id: 1,
+    text: "Hola soy un mensage",
+    author: "Santiago"
+}];
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.status(200).send('Hola mundo!!!');
+});
+
+io.on('connection', function(socket) {
+    console.log('Alguien se ha conectado con socket');
+    socket.emit("messages", messages);
+    socket.on('new-message', function(data){
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
+
+server.listen(8080, function() {
+    console.log('Servidor corriendo en http://localhost:8080');
+});
